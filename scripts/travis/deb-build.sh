ec08361b240e7c6e2927b14dae0eb1836fa4eb9c
Luke Yeager <lukeyeager@users.noreply.github.com>
2016-10-19 16:33:44 -0700

    use travis envvars

--- scripts/travis/deb-build.sh
+++ scripts/travis/deb-build.sh
@@ -10,6 +10,22 @@ set -x
 cd $ROOT_DIR/packaging/deb
 git fetch --tags
 ./build.sh
+
+if [ "$TRAVIS_REPO_SLUG" != "lukeyeager/python-engineio" ]; then
+    echo Skipping PPA upload for fork $TRAVIS_REPO_SLUG
+    exit 0
+fi
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo Skipping PPA upload for pull request
+    exit 0
+fi
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    echo Uploading to ppa for master branch
+fi
+if [ ! -z "$TRAVIS_TAG" ]; then
+    echo Uploading to ppa for a tag
+fi
+
 gpg --import private.key
 cd dist/*
 debsign *source.changes
